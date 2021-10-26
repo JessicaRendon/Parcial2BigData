177c4e0ebdb8afbf81f83d9c9449bf6a3a80aa5b
Miguel <miguelangelroj@hotmail.com>
2021-10-25 19:14:06 -0500

    Corrección de errores, se agrega excepción para días nulos

--- Punto 1/app.py
+++ Punto 1/app.py
@@ -1,11 +1,8 @@
-#importaciones necesarias
-import  boto3, time, wget
+import  boto3, time, wget, urllib
 from datetime import datetime, timedelta 
 
-#definir el S3 usando Boto3
 s3 = boto3.resource('s3')
 
-#se extrae el tiempo del día que se ejecute y se separa en variables día, mes, año
 today = datetime.today()
 day_actual = today.day
 month_actual = today.month
@@ -13,28 +10,29 @@ year_actual = today.year
 
 today.weekday()
 
-#se extraen las keys para descargar la información de Avianca, Ecopetrol, Grupo Aval y Cemento Argos
-array_keys =['AVHOQ', 'EC','AVAL','CMTOY']
+my_stocks =['AVHOQ', 'EC','AVAL','CMTOY']
 
-#función principal 
-def handler(event, context):    
-        if today.weekday() in [0, 1, 2, 3, 4, 5]:
-            get_data()
-            for i in range(len(array_keys)):
-                upload_path = f'stocks/company={array_keys[i]}/year={year_actual}/month={month_actual}/day={day_actual}/{array_keys[i]}.csv'
-                s3.meta.client.upload_file(f'/tmp/{array_keys[i]}.csv', "parcialpunto1download" , upload_path)
-        return{'status':200}
-
-#Esta función permite convertir el formato de fecha a un entero el cual será utilizado para descargar los archivos de la página de yahoo
-#tambien descarga el archivo
 def get_data():
-        today = datetime.today()-timedelta(days=1)
+        today = datetime.today()-timedelta(days=3)
         today = today.replace(hour=13)
         today = int(time.mktime(today.timetuple()))
 
-        for symbol in array_keys:
-            url = f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={today}&period2={today}&interval=1d&events=history&includeAdjustedClose=true"
-            wget.download(url, out = f'/tmp/{symbol}.csv')
+        for symbol in my_stocks:
+            try:
+                url = f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={today}&period2={today}&interval=1d&events=history&includeAdjustedClose=true"
+                wget.download(url, out = f'/tmp/{symbol}.csv')
+            except urllib.error.HTTPError as e:
+                en_ceros = open(f'/tmp/{symbol}.csv','w+')
+                en_ceros.write(f'Date,Open,High,Low,Close,Adj Close,Volume \n{datetime.today().replace(hour=13).strftime("%Y-%m-%d")},0,0,0,0,0,0')
+                en_ceros.close()
+
+def handler(event, context):    
+    if today.weekday() in [0, 1, 2, 3, 4, 5]:
+        get_data()
+        for i in range(len(my_stocks)):
+            upload_path = f'stocks/company={my_stocks[i]}/year={year_actual}/month={month_actual}/day={day_actual}/{my_stocks[i]}.csv'
+            s3.meta.client.upload_file(f'/tmp/{my_stocks[i]}.csv', "parcialpunto1download" , upload_path)
+    return{'status':200}
 
 #para actualizar el athena
 def uploadathena(event, context):
@@ -49,4 +47,3 @@ def uploadathena(event, context):
         },
         WorkGroup='primary'
     )
-
